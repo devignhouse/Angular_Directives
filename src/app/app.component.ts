fa216abf287ca47cf9727e5e90cb22efea4079b2
Nabiel Firfirey <devignhouse@gmail.com>
2021-06-21 14:53:19 +0200

    assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-directives';
+  showSecret: boolean = false;
+
+  log = [];
+
+  checkSecret() {
+    this.showSecret = !this.showSecret;
+    this.log.push(new Date());
+    //this.log.push(this.log.length + 1);
+  }
 }
